8a3adae384bfd40b566f1c8df23ba01a7f03119c
DinoCzar <ryanb_booking@yahoo.com>
2023-10-19 01:42:27 -0700

    Delete files

--- dist/main.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log('tested')
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc3RhcnQtcHJhY3RpY2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ3Rlc3RlZCcpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
